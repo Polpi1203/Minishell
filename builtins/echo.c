67c021e599d4494c9f1119850eb0cac04b67a7f5
Polpi <anthotuigr@gmail.com>
2023-03-30 07:00:38 +0200

    AddBuiltin@

--- builtins/echo.c
+++ builtins/echo.c
@@ -1,24 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: polpi <polpi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/29 09:44:26 by polpi             #+#    #+#             */
-/*   Updated: 2023/03/29 09:44:46 by polpi            ###   ########.fr       */
+/*   Created: 2023/03/30 06:59:40 by polpi             #+#    #+#             */
+/*   Updated: 2023/03/30 07:00:16 by polpi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct s_token
-{
-	int					type;
-	int					flag_env; //if == 1, then use env variables
-	int					i;
-	int					fdwrite;
-	int					fdread;
-	char				*cmd;
-	char				**arg;
-	struct s_token		*next;
-	struct s_token		*prev;
-}				t_token;
--- minishell.c
+++ minishell.c
@@ -6,7 +6,33 @@
 /*   By: polpi <polpi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 11:39:01 by polpi             #+#    #+#             */
-/*   Updated: 2023/03/27 11:39:57 by polpi            ###   ########.fr       */
+/*   Updated: 2023/03/30 07:00:16 by polpi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+
+int	main(int ac, char **av, char **env)
+{
+	char			*input;
+	struct termios	termios;
+
+	(void)ac;
+	(void)av;
+	(void)env;
+	if ((tcgetattr(STDIN_FILENO, &termios)) == -1)
+		exit(EXIT_FAILURE);
+	termios.c_lflag &= ~(ECHOCTL);
+	if ((tcsetattr(STDIN_FILENO, TCSANOW, &termios)) == -1)
+		exit(EXIT_FAILURE);
+	input = "lol";
+	while (input != NULL)
+	{
+		signals_init();
+		input = readline("Minishell > ");
+		parser(input);
+		add_history(input);
+	}
+	free(input);
+	return (0);
+}
